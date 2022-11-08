43e035021a2c1f4fe6e5a1f7c109caf592357f4e
IvanKoshel98 <GitHubPassvord142>
2022-11-08 19:12:57 +0200

    add per_page in param's obj

--- src/js/pixabayReq.js
+++ src/js/pixabayReq.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 const PIXABAY_KEY = '31151048-14715764b2774648f52159790';
 const URL = 'https://pixabay.com/api/';
 
-export default pixabayReq = searchQuery => {
+export default pixabayReq = (searchQuery,page) => {
   const params = {
     key: '31151048-14715764b2774648f52159790',
     url: 'https://pixabay.com/api/',
     q: searchQuery,
+    per_page:10
   };
-  axios.get(URL, { params }).then(res=>res.json()).then(console.log);
+  axios.get(URL, { params }).then(res=>res.data.hits).then(console.log);
 };
